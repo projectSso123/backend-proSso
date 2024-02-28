e7a87ed6fda0740e953fe87d85e4b26c3445fc16
Rohan <rohanb158@gmail.com>
2024-02-28 14:05:19 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# syntax=docker/dockerfile:1
+
+# Comments are provided throughout this file to help you get started.
+# If you need more help, visit the Dockerfile reference guide at
+# https://docs.docker.com/go/dockerfile-reference/
+
+ARG NODE_VERSION=18.17.0
+
+FROM node:${NODE_VERSION}-alpine
+
+
+
+WORKDIR /backend
+
+# Download dependencies as a separate step to take advantage of Docker's caching.
+# Leverage a cache mount to /root/.npm to speed up subsequent builds.
+# Leverage a bind mounts to package.json and package-lock.json to avoid having to copy them into
+# into this layer.
+
+
+# Copy the rest of the source files into the image.
+COPY . .
+
+# Expose the port that the application listens on.
+EXPOSE 8080
+
+Run npm i
+# Run the application.
+CMD ["node" ,"index.js"]
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import bodyParser from "body-parser"
+import bcrypt from 'bcrypt'
+import cors from 'cors'
+const saltRound = 10
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('out'));
+app.use(express.json())
+// app.engine('jsx', reactViews.createEngine());
+app.get('/initiate-auth',(req,res)=>{
+    res.redirect("/")
+})
+
+app.post("/api/login",async(req,res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    var hash;
+    try{
+        hash = await bcrypt.hash(password,saltRound)
+    }
+    catch(err){
+    return res.json({"success":false,"message":err}).status(300)
+    }
+    console.log(hash)
+    return res.json(hash)
+    // res.setHeader('Authorization', `Bearer ${hash}`)
+    // res.redirect(`http://localhost:3000/?token=${token}`)
+})
+app.post("/test2",(req,res)=>{
+    const email = req.body.email;
+    console.log(email)
+    res.json({email:"email"})
+})
+app.listen(8080,()=>{
+    console.log("server is running on 8080")
+})
