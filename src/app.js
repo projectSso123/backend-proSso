222e8e85be1fa8d236e00b2c0fd18a3b272ba324
Rohan <rohanb158@gmail.com>
2024-04-08 10:20:30 +0530

    fixed cors error

--- src/app.js
+++ src/app.js
@@ -15,7 +15,8 @@ const app = express()
 app.use(cors({
   "origin": ["http://localhost:3000",
   "http://localhost:3001",
-  "http://localhost:8080"
+  "http://localhost:8080",
+  "https://rtvsso.onrender.com"
 ],
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE", 
   "credentials":true
