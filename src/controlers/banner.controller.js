65fd0e73cc4fe5d0373d04c50a9af33340d10f4f
Rohan <rohanb158@gmail.com>
2024-08-26 00:35:14 +0530

    restoring the project

--- src/controlers/banner.controller.js
+++ src/controlers/banner.controller.js
@@ -2,18 +2,47 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/uploadOncloudinary.js";
 import { Banner } from "../model/banner.model.js";
-
+import { User } from "../model/user.model.js";
+import { SuperAdmin } from "../model/super.admin.js";
 const addBanner = asyncHandler(async(req,res)=>{
-    const localpath = req.files?.bannner[0]?.path
+    const localpath = req.file?.path
+    console.log("this -> ",req.file)
     const cloudPath = await uploadOnCloudinary(localpath)
     console.log(cloudPath)
     if(!cloudPath){
         throw new ApiError(401,"upload falied")
     }
     const banner = await Banner.create({
-        url:cloudPath,
+        url:cloudPath.url,
         broadcast:true,
     })
     return res.status(200).json("uploaded successfull")
 })
-export {addBanner}
+const getBanners = asyncHandler(async(req, res)=>{
+    const sessionUser = req.session.user;
+    const user = await User.findOne({email:sessionUser.email}) 
+    if(!user){
+        throw new ApiError(401, "user not found")
+   }
+   const superAdmin = await SuperAdmin.findOne({user:user._id});
+   if(!superAdmin){
+    throw new ApiError(403,"unauthorized")
+   }
+   const banners = await Banner.find({broadcast:true}).exec()
+   return res.status(200).json(banners)
+})
+const deletebanner = asyncHandler(async(req, res)=>{
+    const {_id} = req.body;
+    const sessionUser = req.session.user;
+    const user = await User.findOne({email:sessionUser.email}) 
+    if(!user){
+        throw new ApiError(401, "user not found")
+   }
+   const superAdmin = await SuperAdmin.findOne({user:user._id});
+   if(!superAdmin){
+    throw new ApiError(403,"unauthorized")
+   }
+   const banner = await Banner.deleteOne({_id:_id});
+   return res.status(200).json(banner)
+})
+export {addBanner,getBanners,deletebanner}
--- src/routes/banner.route.js
+++ src/routes/banner.route.js
@@ -1,6 +1,8 @@
 import { upload } from "../middleware/multer.middleware.js";
 import express from 'express'
-import { addBanner } from "../controlers/banner.controller.js";
+import { addBanner,getBanners,deletebanner } from "../controlers/banner.controller.js";
 const router = express.Router();
 router.route("/v1/broadcast/addbanner").post(upload.single('banner'),addBanner)
+router.route("/v1/broadcast/getbanner").post(getBanners)
+router.route("/v1/broadcast/deletebanner").post(deletebanner)
 export default router
